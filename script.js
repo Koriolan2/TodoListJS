4d21b1107798128133a444d24f7d8abd787aae74
Kozmin Jurii <Koriolan2@gmail.com>
2020-12-07 22:22:07 +0200

    Часть домашнего задания до работі с LocalStorage

--- script.js
+++ script.js
@@ -36,17 +36,28 @@ const render = function () {
        
         const btnComplete = li.querySelector('.todo-complete');
 
-        btnComplete.addEventListener('click', function() {
+        btnComplete.addEventListener('click', function () {
             
             item.completed = !item.completed;
             render();
         });
+
+        const btnRemove = li.querySelector('.todo-remove');
+
+        btnRemove.addEventListener('click', function () {
+            btnRemove.closest('.todo-item').remove();
+        });
+
+        headerInput.value = '';
     });
 }
 
 todoControl.addEventListener('submit', function(event) {
     event.preventDefault();
-
+    if (headerInput.value == '') {
+        alert('Пустое значение');
+        return;
+    }
     const newTodo = {
         value:headerInput.value,
         completed:false
